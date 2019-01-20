a43a4a3cb1171400dc0195299cb73e1939dd6270
Alexander Kettler <alexander.kettler91@gmail.com>
2019-01-20 17:58:07 +0100

    Launch new layout
    - remove some unused assets again
    
    + add new main java for launching the new layout
    
    # update some layout details

---
+++ app/src/main/java/com/example/aklesoft/heartrate_monitor/MainSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.aklesoft.heartrate_monitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSettingsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_settings);
+    }
+}
