631a0c67586005a9f9632b5ca462bc3cc67a7c52
Alexander Kettler <alexander.kettler91@gmail.com>
2019-02-15 10:19:27 +0100

    Orientation Settings
    # let them look similar to the rest of the settings items

--- app/src/main/java/com/example/aklesoft/heartrate_monitor/MainSettingsActivity.java
+++ app/src/main/java/com/example/aklesoft/heartrate_monitor/MainSettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.aklesoft.heartrate_monitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainSettingsActivity extends AppCompatActivity
 {
@@ -11,5 +13,10 @@ public class MainSettingsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerOrientation);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.blackmode_orienntations, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
