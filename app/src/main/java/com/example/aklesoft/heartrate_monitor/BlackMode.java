93897c28213f961aa6f868ce71e69d09e9f8095e
kleinandreas2002 <kleinandreas2002@gmail.com>
2020-04-15 10:40:57 +0200

    + change default zoom to 17.0

--- app/src/main/java/com/example/aklesoft/heartrate_monitor/BlackMode.java
+++ app/src/main/java/com/example/aklesoft/heartrate_monitor/BlackMode.java
@@ -309,7 +309,7 @@ public class BlackMode extends Activity implements GoogleApiClient.ConnectionCal
 
 
             IMapController mapController = map.getController();
-            mapController.setZoom(18.0);
+            mapController.setZoom(17.0);
             mapController.setCenter(gpCurrentGpsPosition);
 
 
